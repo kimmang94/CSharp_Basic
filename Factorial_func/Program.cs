f414e0c968991f579fb7ef58ec2e89184a574d38
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-04 00:23:24 +0900

    재귀함수를 사용한 팩토리얼

---
+++ Factorial_func/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+       static void Main(string[] args)
+        {
+
+            // 5! = 5 * (4!)
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
+        }
+    }
+}
