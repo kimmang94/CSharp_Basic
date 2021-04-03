e76481288cec719b4fb73006bd7f8d6ad7419380
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-03 19:03:14 +0900

    Swap 사용하기
    ref 참조와 복사의 차이점

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,25 +4,41 @@ namespace CSharp
 {
     class Program
     {
-        // Method , Function
-        /*
-         * 한정자 반환형식 이름(매개변수목록)
-         * {
-         * }
-         */
 
-        static void HelloWorld()
+        static void Swap(ref int a, ref int b)
         {
-            Console.WriteLine("Hello World!");
+            int temp = a;
+            a = b;
+            b = temp;
+
         }
 
+        static void AddOne(ref int number)
+        {
+            number = number + 1;
+        }
+        static int AddOne2(int number)
+        {
+            return number + 1;
+        }
 
-        // 덧셈 함수
-
-       
         static void Main(string[] args)
         {
-            Console.WriteLine(Caculater.Add(2, 3));
+            // 복사(짝퉁) ref : 참조(진퉁)
+            int a = 0;
+            Program.AddOne(ref a);
+
+            Console.WriteLine(a);
+            // 아래 버전이 더좋다 
+            a = Program.AddOne2(a);
+
+            Console.WriteLine(a);
+
+            int num1 = 1;
+            int num2 = 2;
+            Program.Swap(ref num1, ref num2);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
         }
     }
 }
