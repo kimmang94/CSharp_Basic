97508019989a0aaeb1d3bb9edb0a9ed3cce23732
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-03 15:49:15 +0900

    계산기 함수로만들어서 사용해보기

---
+++ CSharp/Caculater.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Caculater
+    {
+
+        public static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+        public static float Add(float a, float b)
+        {
+            float result = a + b;
+            return result;
+        }
+        public static float Add(int a, float b)
+        {
+            float result = a + b;
+            return result;
+
+        }
+
+        public static int Minus(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+        public static float Minus(float a, float b)
+        {
+            float result = a - b;
+            return result;
+        }
+        public static float Minus(int a, float b)
+        {
+            float result = a - b;
+            return result;
+
+        }
+
+        public static int Devide(int a, int b)
+        {
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("0 으로 나눌수 없습니다");
+            }
+            int result = a / b;
+            return result;
+        }
+        public static float Devide(float a, float b)
+        {
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("0 으로 나눌수 없습니다");
+            }
+                float result = a / b;
+                return result;
+            
+        }
+        public static float Devide(int a, float b)
+        {
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("0 으로 나눌수 없습니다");
+            }
+
+            float result = a / b;
+            return result;
+        }
+
+        public static int Mult(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+        public static float Mult(int a, float b)
+        {
+            float result = a * b;
+            return result;
+        }
+        public static float Mult(float a, float b)
+        {
+            float result = a * b;
+            return result;
+        }
+
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        // Method , Function
+        /*
+         * 한정자 반환형식 이름(매개변수목록)
+         * {
+         * }
+         */
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        // 덧셈 함수
+
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Caculater.Add(2, 3));
+        }
+    }
+}
