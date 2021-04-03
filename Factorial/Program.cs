b3166be507751973f3912ab7b501d6e820d2a89d
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-03 22:15:37 +0900

    재귀함수를 사용하지 않은 팩토리얼

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            int ret = 1;
+            for (int num = 1; num <= n; num++)
+            {
+                ret *= num;
+            }
+
+            return ret;
+        }
+       static void Main(string[] args)
+        {
+
+            // n! = n * (n-1) * ... * 1
+            int ret = Factorial(5);
+        }
+    }
+}
