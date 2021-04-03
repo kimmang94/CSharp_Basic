a972f011dfc8e3f57f082340a0b5f21f0c4bc551
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-03 22:01:14 +0900

    구구단 예제

---
+++ Gugudan/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        // 오버로딩 함수 이름의 재사용
+       
+       static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++ )
+            {
+                Console.WriteLine( $"{i}단");
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} X {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
