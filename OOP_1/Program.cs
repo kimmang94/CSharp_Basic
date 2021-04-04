c18fe9d519f67951cfaf8ee93d20875fe524f2da
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-05 02:21:52 +0900

    OOP 기초 1

---
+++ OOP_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharp
+{
+    // 객체 위주로 생각 (OOP : Object Oriented Programming)
+    // 코드내의 모든 것
+    // 객체 설계
+    
+    // 어떤방식으로 묘사해야함
+    // Knight
+    // 속성 : HP, Attack, Position, ... 
+    // 기능 : Move, Attack, Die, ... 
+    
+    class Knight
+    {
+        public int hp;
+        public int attack;
+
+
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Attack");
+        }
+    }
+
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+
+            knight.hp = 100;
+            knight.attack = 10;
+
+            knight.Move();
+            knight.Attack();
+        }
+    }
+}
