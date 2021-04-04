7374551572c03370c7711ac56b1bd3cdd919aec6
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-05 03:29:43 +0900

    OOP_2

---
+++ OOP_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharp
+{
+    // 참조
+    class Knight
+    {
+        public int hp;
+        public int attack;
+
+
+        public void Move()
+        {
+            Console.WriteLine("Knight Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Knight Attack");
+        }
+    }
+    // 복사
+    struct Mage
+    {
+        public int hp;
+        public int attack;
+    }
+
+ 
+    class Program
+    {
+        static void KillMage(Mage mage)
+        {
+            mage.hp = 0;
+        }
+
+        static void KillKnight(Knight knight)
+        {
+            knight.hp = 0;
+        }
+        static void Main(string[] args)
+        {
+            Mage mage;
+            mage.hp = 100;
+            mage.attack = 50;
+            KillMage(mage);
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 10;
+            knight.Move();
+            knight.Attack();
+            KillKnight(knight);
+        }
+    }
+}
